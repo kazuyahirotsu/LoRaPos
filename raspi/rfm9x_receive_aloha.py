ef4b44d92d8a88b49b991a748b1440e2d477bb75
kazuyahirotsu <hrtkzy4444@gmail.com>
2023-07-03 18:21:28 +0200

    aloha beacon test

---
+++ raspi/rfm9x_receive_aloha.py
@@ -0,0 +1,38 @@
+import board
+import busio
+import digitalio
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.CE1)
+reset = digitalio.DigitalInOut(board.D25)
+
+import adafruit_rfm9x
+import time
+
+rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
+print(rfm9x.ack_retries, rfm9x.spreading_factor, rfm9x.signal_bandwidth, rfm9x.bw_bins)
+rfm9x.ack_retries = 0
+rfm9x.spreading_factor = 7 
+rfm9x.signal_bandwidth = 250000
+rfm9x.enable_crc = False
+print(rfm9x.ack_retries, rfm9x.spreading_factor, rfm9x.signal_bandwidth)
+
+packets = []
+
+while True:
+    packet = rfm9x.receive()
+    received_time = time.time_ns()
+    print("time: ", received_time)
+    if packet is None:
+        # Packet has not been received
+        print("Received nothing! Listening again...")
+    else:
+        # Received a packet!
+        print("Received (raw bytes): {0}".format(packet))
+
+        packet_text = str(packet, "ascii")
+        print("Received (ASCII): {0}".format(packet_text))
+        rssi = rfm9x.last_rssi
+        print("Received signal strength: {0} dB".format(rssi))
+
+        packets.append([received_time, packet_text, rssi])
+
---
+++ raspi/rfm9x_send_aloha.py
@@ -0,0 +1,47 @@
+import board
+import busio
+import digitalio
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.CE1)
+reset = digitalio.DigitalInOut(board.D25)
+
+import adafruit_rfm9x
+import time
+import random
+
+rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
+print(rfm9x.ack_retries, rfm9x.spreading_factor, rfm9x.signal_bandwidth, rfm9x.bw_bins)
+rfm9x.ack_retries = 0
+rfm9x.spreading_factor = 7
+rfm9x.signal_bandwidth = 250000
+print(rfm9x.ack_retries, rfm9x.spreading_factor, rfm9x.signal_bandwidth)
+data_to_send = bytes("hello\r\n","utf-8")
+
+def send_data_with_aloha(data):
+
+    while True:
+        rfm9x.send(data)
+        print(f"Data sent: {data}")
+        
+        # Check for an acknowledgment from the receiver
+        packet = rfm9x.receive(timeout=1.0)  # adjust timeout as necessary
+        if packet is not None:
+            packet_text = str(packet, 'ascii')
+            if packet_text == "ACK":
+                print("Acknowledgment received.")
+                sent = True
+                return
+            else:
+                print("Unexpected packet received: {packet_text}. Retransmitting data...")
+        else:
+            print("No acknowledgment received. Retransmitting data...")
+        
+        # Wait for a random period before retransmitting to reduce the chance of collisions
+        wait_time = random.uniform(0, 1)
+        print("wait for ", wait_time)
+        time.sleep(wait_time)
+
+while True:
+    send_data_with_aloha(data_to_send)
+    print(data_to_send)
+    time.sleep(0.5)
